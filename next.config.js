03eb0a7e3511689f9202e18c2ffaaebc626099ea
April1029 <jj5rb@virginia.edu>
2026-03-25 00:07:47 -0700

    fix: remove basePath for custom domain

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "jingjingji.me";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
   images: {
     unoptimized: true,
   },
